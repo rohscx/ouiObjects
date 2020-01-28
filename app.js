97ac168ae15babab27e606d0a80a27797188d997
rohscx <rohscx@gmail.com>
2020-01-28 12:11:32 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+const clipboardy = require("clipboardy");
+
+(async function () {
+
+     
+    const result = await fetch('https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf');
+    const data = await result.text();
+    const ouiData = data
+        .split("\n")
+        .filter((f) => f.search(new RegExp(/^#/)) != 0)
+        .filter((f) => f.length > 0)
+        .reduce((n,o) => {
+              const splitData = o.split('\t');
+              const index0 = splitData.shift();
+              const remainingData = splitData.join(' ');
+              const objectDate = {
+                  oui: index0,
+                  vendor: remainingData,
+              }
+              n.push(objectDate);
+              return n;
+        },[])//?
+    const asString = JSON.stringify(ouiData);
+    clipboardy.writeSync(asString)
+})()
